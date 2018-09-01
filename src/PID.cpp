4a710fb02efd2b3498f854ca3b4a4915b1944dd2
Jakub Prüher <jacobnzw@gmail.com>
2018-09-01 19:41:37 +0200

    Starting to tune the PID controller.

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,25 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp, double Ki, double Kd)
+{
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
+
+  p_error_ = 0;
+  i_error_ = 0;
+  d_error_ = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+  d_error_ = cte - p_error_; // difference with the past error
+  p_error_ = cte;
+  i_error_ += cte; // accumulating past errors
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+  return Kp_*p_error_ + Ki_*i_error_  + Kd_*d_error_;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,17 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-std::string hasData(std::string s) {
+std::string hasData(std::string s)
+{
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.find_last_of("]");
-  if (found_null != std::string::npos) {
+  if (found_null != std::string::npos)
+  {
     return "";
   }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
+  else if (b1 != std::string::npos && b2 != std::string::npos)
+  {
     return s.substr(b1, b2 - b1 + 1);
   }
   return "";
@@ -33,7 +36,10 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  double Kp = 0.0001;
+  double Ki = 0.00001;
+  double Kd = 0.05;
+  pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -42,10 +48,12 @@ int main()
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
       auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
+      if (s != "")
+      {
         auto j = json::parse(s);
         std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
+        if (event == "telemetry")
+        {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
@@ -57,7 +65,11 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+         pid.UpdateError(cte);
+         steer_value = -pid.TotalError();
+         if (steer_value > 1) steer_value = 1;
+         if (steer_value < -1) steer_value = -1;
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
@@ -68,7 +80,9 @@ int main()
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
-      } else {
+      }
+      else
+      {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
